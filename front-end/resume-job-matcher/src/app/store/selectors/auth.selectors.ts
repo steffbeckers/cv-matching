355c9682d8ee0b496922ba58e62a6fa1f35d9e3d
Steff Beckers <steff@steffbeckers.eu>
2020-03-23 00:04:50 +0100

    Ran prettier

--- front-end/resume-job-matcher/src/app/store/selectors/auth.selectors.ts
+++ front-end/resume-job-matcher/src/app/store/selectors/auth.selectors.ts
@@ -1,10 +1,19 @@
 import { createSelector } from '@ngrx/store';
 import { getAppState, AppState, AuthState } from '../reducers';
 
-export const getAuthState = createSelector(getAppState, (state: AppState) => state.auth);
+export const getAuthState = createSelector(
+  getAppState,
+  (state: AppState) => state.auth
+);
 
-export const getUser = createSelector(getAuthState, (state: AuthState) => state.user);
-export const getToken = createSelector(getAuthState, (state: AuthState) => state.token);
+export const getUser = createSelector(
+  getAuthState,
+  (state: AuthState) => state.user
+);
+export const getToken = createSelector(
+  getAuthState,
+  (state: AuthState) => state.token
+);
 export const getAuthenticated = createSelector(
   getAuthState,
   (state: AuthState) => state.authenticated
