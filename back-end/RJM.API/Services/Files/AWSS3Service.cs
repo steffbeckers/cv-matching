7b6fc66273a7ed7213cc24f72f8d4b8c1f166081
Steff Beckers <steff@steffbeckers.eu>
2020-04-07 22:56:32 +0200

    Document upload working to AWS

---
+++ back-end/RJM.API/Services/Files/AWSS3Service.cs
@@ -0,0 +1,55 @@
+﻿using Amazon;
+using Amazon.Runtime;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.S3.Transfer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RJM.API.Services.Files
+{
+    public class AWSS3Service
+    {
+        private readonly IConfiguration configuration;
+        private readonly ILogger<AWSS3Service> logger;
+        private readonly AmazonS3Client awsS3Client;
+
+        public AWSS3Service(IConfiguration configuration, ILogger<AWSS3Service> logger)
+        {
+            this.configuration = configuration;
+            this.logger = logger;
+            this.awsS3Client = new AmazonS3Client(
+                this.configuration.GetSection("FileService")
+                    .GetSection("AWSS3Service")
+                    .GetValue<string>("AccessKey"),
+                this.configuration.GetSection("FileService")
+                    .GetSection("AWSS3Service")
+                    .GetValue<string>("SecretAccessKey"),
+                RegionEndpoint.EUCentral1
+            );
+        }
+
+        public async Task UploadFile(Stream fileStream, string key, string mimeType)
+        {
+            TransferUtility fileTransferUtility = new TransferUtility(this.awsS3Client);
+
+            TransferUtilityUploadRequest uploadRequest = new TransferUtilityUploadRequest() {
+                InputStream = fileStream,
+                BucketName = this.configuration.GetSection("FileService")
+                                .GetSection("AWSS3Service")
+                                .GetSection("Bucket")
+                                .GetValue<string>("Name"),
+                Key = key,
+                ContentType = mimeType,
+                AutoCloseStream = true
+            };
+
+            await fileTransferUtility.UploadAsync(uploadRequest);
+        }
+    }
+}
---
+++ back-end/RJM.API/Services/Files/FileService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using RJM.API.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RJM.API.Services.Files
+{
+    public class FileService
+    {
+        private readonly IConfiguration configuration;
+        private readonly ILogger<FileService> logger;
+        private readonly AWSS3Service awsS3Service;
+
+        public FileService(
+            IConfiguration configuration,
+            ILogger<FileService> logger,
+            AWSS3Service awsS3Service
+        )
+        {
+            this.configuration = configuration;
+            this.logger = logger;
+            this.awsS3Service = awsS3Service;
+        }
+
+        public async Task UploadDocument(Document document, Stream stream)
+        {
+            string uploadLocation = this.configuration.GetSection("FileService")
+                                        .GetValue<string>("Location");
+            switch (uploadLocation)
+            {
+                case "AWSS3":
+                    string pathInBucket = this.configuration.GetSection("FileService")
+                                              .GetSection("AWSS3Service")
+                                              .GetSection("Bucket")
+                                              .GetValue<string>("DocumentsPath");
+                    pathInBucket += "/" + document.Id.ToString().ToUpper();
+
+                    await this.awsS3Service.UploadFile(stream, pathInBucket, document.MimeType);
+                    break;
+                case "AzureStorage":
+                    break;
+            }
+        }
+    }
+}
