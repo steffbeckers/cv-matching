67bae4e2c45be3021c78f538ad65ee69e1966d45
Steff Beckers <steff@steffbeckers.eu>
2020-04-22 00:08:11 +0200

    Check on unauthorized request => logout

--- front-end/vue/resume-job-matcher/src/plugins/axios.js
+++ front-end/vue/resume-job-matcher/src/plugins/axios.js
@@ -3,8 +3,28 @@
 import Vue from 'vue';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+import store from '../store';
 
 Vue.use(VueAxios, axios);
 
 // API endpoint
 Vue.axios.defaults.baseURL = process.env.VUE_APP_API;
+
+Vue.axios.interceptors.response.use(
+  function(response) {
+    return response;
+  },
+  function(error) {
+    // If request status is 0 (no connection to API)
+    if (error.request.status === 0) {
+      return Promise.reject({ message: 'Cannot create a connection to the server. Please check your network connection.' });
+    }
+
+    // Log out on unauthorized
+    if (error.response && error.response.status === 401) {
+      store.dispatch('auth/logout');
+    }
+
+    return Promise.reject(error);
+  }
+);
